5c63be499e4c92ecb05f280063d4b47cd8e9ac69
Cook1619 <cook6348@gmail.com>
2019-11-02 22:57:57 -0500

    Adding parameters into graphql queries

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import { Link } from "react-router-dom";
+import { link } from "fs";
 
 export default class Posts extends Component {
   render() {
     return (
-      <Query query={POSTS_QUERY}>
-        {({ loading, data }) => {
-          if (loading) return 'Loading...';
-          const { posts } = data;
-          return posts.map(post => (
-            <Link key={post.id} to={`/post/${post.id}`}>
-              <h1>{post.title}</h1>
-            </Link>
-          ));
-        }}
-      </Query>
+      <ul>
+        <Query query={POSTS_QUERY}>
+          {({ loading, data }) => {
+            if (loading) return "Loading...";
+            const { posts } = data;
+            return posts.map(post => (
+              <li key={post.id}>
+                <Link to={`/post/${post.id}`}>{post.title}</Link>
+              </li>
+            ));
+          }}
+        </Query>
+      </ul>
     );
   }
 }
